370ffdfe39ec1915b4d92bc2cbaa135db5d32dc9
Andrés Ortega <andres03ortega@gmail.com>
2025-08-05 19:12:22 +0200

    cmabio correo

--- pages/api/sentEmail.js
+++ pages/api/sentEmail.js
@@ -54,7 +54,7 @@ export default async function handler(req, res) {
 
   const mailOptions = {
     from: process.env.SMTP_USER,
-    to: 'andres03ortega@gmail.com, norainesmontoya@hotmail.com',
+    to: 'andres03ortega@gmail.com, norainesmontoya@hotmail.com, norainesmontoya67@gmail.com',
     subject: `NUEVO TRABAJO para ${name}`,
     html: `
       <p>Nombre: ${name}</p>
